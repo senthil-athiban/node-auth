f456ffe284cda96a13a5b526828b573a2af980c2
Senthil Athiban <senthilathiban0@gmail.com>
2024-11-25 22:34:31 +0530

    (feat) add token while logging in

--- server/routers/login.ts
+++ server/routers/login.ts
@@ -1,54 +1,71 @@
 import { Request, Response, Router } from "express";
 import UserModel from "../models/userSchema";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
 router.post("/signup", async (req: Request, res: Response): Promise<any> => {
-    // Handles user sign-up.
-    const { username, password } = req.body;
+  // Handles user sign-up.
+  const { username, password } = req.body;
 
+  const existingUser = await UserModel.findOne({
+    where: {
+      username,
+    },
+  });
 
-    const existingUser = await UserModel.findOne({
-        where: {
-            username
-        }
-    });
-    
-    if(existingUser) return res.status(400).json({ message: "User Already Exists" });
-    
-    const hashedPassword = await bcryptjs.hash(password, 10);
+  if (existingUser)
+    return res.status(400).json({ message: "User Already Exists" });
 
-    const user = new UserModel({username, password: hashedPassword});
-    const response = await user.save();
+  const hashedPassword = await bcryptjs.hash(password, 10);
 
-    return res.status(200).json({
-        message: "User Created Successfully",
-        user
-    });
+  const user = new UserModel({ username, password: hashedPassword });
+  await user.save();
+
+  return res.status(200).json({
+    message: "User Created Successfully",
+    user,
+  });
 });
 
-router.post("/login", async (req: Request, res: Response) : Promise<any> => {
-    const { username, password }  = req.body;
+router.post("/login", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const { username, password } = req.body;
 
     const userExists = await UserModel.findOne({
-        where: {
-            username
-        }
+      where: {
+        username,
+      },
     });
 
-    if(!userExists) {
-        return res.status(404).json({message: "No user found"});
+    if (!userExists) {
+      return res.status(404).json({ message: "No user found" });
     }
 
-    const comparePassword = await bcryptjs.compare(password, userExists.password);
+    const comparePassword = await bcryptjs.compare(
+      password,
+      userExists.password
+    );
 
-    if(!comparePassword) {
-        return res.status(401).json({message: "Invalid credentials"});
+    if (!comparePassword) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
+
+    const token = jwt.sign(
+      { userId: userExists._id },
+      process.env.JWT_SECRET_KEY!,
+      {
+        expiresIn: "1hr",
+      }
+    );
 
     return res.status(200).json({
-        message: "Login succesfully",
-        user: userExists
-    })
-})
+      message: "Login succesfully",
+      user: userExists,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Login failed" });
+  }
+});
