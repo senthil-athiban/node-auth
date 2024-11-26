4989871d40abbbd3f942e9cecae7ca14a4e8b60a
Senthil Athiban <senthilathiban0@gmail.com>
2024-11-27 01:56:44 +0530

    (feat) add refresh token in db and refres api

--- server/middleware/userAuth.ts
+++ server/middleware/userAuth.ts
@@ -2,13 +2,13 @@ import { NextFunction, Response, Request } from "express";
 import jwt from 'jsonwebtoken';
 require('dotenv').config()
 
-export const userAuth = async (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     const token = req.header('Authorization');
     if(!token) {
         return res.status(401).json({error: 'Access denied'});
     }
     try {
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY!);
+        const decodedToken = jwt.verify(token, process.env.ACCESS_SECRET_KEY!);
         //@ts-ignore
         req.userId = decodedToken.userId;
         next();
--- server/routers/user.ts
+++ server/routers/user.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import UserModel from "../models/userSchema";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { authMiddleware } from "../middleware/userAuth";
 require('dotenv').config();
 
 const router = Router();
@@ -9,11 +10,7 @@ const router = Router();
 router.post("/signup", async (req: Request, res: Response): Promise<any> => {
   // Handles user sign-up.
   const { username, password } = req.body;
-  const existingUser = await UserModel.findOne({
-    where: {
-      username,
-    },
-  });
+  const existingUser = await UserModel.findOne({username});
 
   if (existingUser)
     return res.status(400).json({ message: "User Already Exists" });
@@ -49,18 +46,22 @@ router.post("/login", async (req: Request, res: Response): Promise<any> => {
     }
 
     const accessToken = jwt.sign(
-      { userId: userExists._id },
+      { userId: userExists.username },
       process.env.ACCESS_SECRET_KEY!,
       {
-        expiresIn: "15s",
+        expiresIn: "1m",
       }
     );
 
     const refreshToken = jwt.sign(
-      {userId: userExists._id},
+      {userId: userExists.username},
       process.env.REFRESH_SECRET_KEY!,
       { expiresIn: '1hr'}
     );
+
+    const hashedRefreshToken = await bcryptjs.hash(refreshToken, 10);
+
+    await UserModel.findOneAndUpdate({username}, {refreshtoken: hashedRefreshToken}, {new: true});
 
     res.cookie('jwt', refreshToken, {
       httpOnly: true,
@@ -68,7 +69,7 @@ router.post("/login", async (req: Request, res: Response): Promise<any> => {
       sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60  * 1000
     });
-    
+
     return res.status(200).json({
       message: "Login succesfully",
       user: userExists,
@@ -79,5 +80,66 @@ router.post("/login", async (req: Request, res: Response): Promise<any> => {
   }
 });
 
+
+router.post("/refresh", async (req: Request, res: Response) => {
+  try {
+    const refreshToken = req.cookies.jwt;
+
+    if(!refreshToken) {
+      res.status(401).json({error: 'Un authorized'});
+    }
+
+    const decodedToken = jwt.verify(
+      refreshToken,
+      process.env.REFRESH_SECRET_KEY!
+    );
+
+    //@ts-ignore
+    const userId = decodedToken.userId;
+    const user = await UserModel.findOne({username: userId});
+    if(!user) {
+      res.status(404).json({message: 'No user found'});
+    }
+    const newAccessToken = jwt.sign(
+      { userId: user?.username },
+      process.env.ACCESS_SECRET_KEY!,
+      {
+        expiresIn: "15m",
+      }
+    );
+
+    const newRefreshToken = jwt.sign(
+      {userId: user?.username},
+      process.env.REFRESH_SECRET_KEY!,
+      { expiresIn: '1hr'}
+    );
+
+    await UserModel.findOneAndUpdate({username: user?.username}, {refreshtoken: newRefreshToken}, {new: true});
+
+    res.cookie('jwt', newRefreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 7 * 24 * 60 * 60  * 1000
+    });
+
+    res.status(201).json({
+      message: "Generated new access + refresh token",
+      user,
+      newAccessToken
+    });
+
+  } catch (error) {
+    res.status(403).json({message: 'Invalid token'});
+  }
+})
+
+router.get("/check", authMiddleware, (req: Request, res: Response): void => {
+  try {
+    res.status(201).json({ message: "you are verified" });
+  } catch (error) {
+    res.status(500).json({ error: "Login failed" });
+  }
+})
 
 export const userRouter = router;
