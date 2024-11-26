ec54640faca9d41c2d054fab77fe9b4ad46c1553
Senthil Athiban <senthilathiban0@gmail.com>
2024-11-27 01:17:31 +0530

    (feat): add cors

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import connectDb from "./db/db";
 import {userRouter} from "./routers/user";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use("/api/v1/user", userRouter);
 app.listen(8080, () => {
