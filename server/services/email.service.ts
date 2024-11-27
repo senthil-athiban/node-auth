26f3e516ac307d2e900c8df3ca614d310f25b694
Senthil Athiban <senthilathiban0@gmail.com>
2024-11-27 21:45:58 +0530

    (fix) domain

--- server/services/email.service.ts
+++ server/services/email.service.ts
@@ -34,7 +34,7 @@ If you did not request any password resets, then ignore this email.`;
 
 const sendEmailVerification = async (to: string, token: string) => {
   const subject = "Email Verification";
-  const verificationEmailUrl = `http://localhost:8080/verify-email?token=${token}`;
+  const verificationEmailUrl = `${process.env.DOMAIN}/verify-email?token=${token}`;
   const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
