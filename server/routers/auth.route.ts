cbae0e8b1cd5a37df4e3b5e083453af27e9beb9a
Senthil Athiban <senthilathiban0@gmail.com>
2024-11-27 18:17:29 +0530

    (feat) add forgot password and reset password

--- server/routers/auth.route.ts
+++ server/routers/auth.route.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/userMiddleware";
 import { login, refreshToken, signup, verifyUser, sendEmailVerification, verifyEmail } from "../controller/authController";
+import UserModel from "../models/userSchema";
+import moment from "moment";
+import { tokenTypes } from "../config/tokens";
+import { Token } from "../models/tokenSchema";
+import ApiError from "../utils/apiError";
 require('dotenv').config();
+
+
+import jwt from "jsonwebtoken";
+import {Request, Response} from "express";
+import { emailService } from "../services";
 
 const router = Router();
 
@@ -11,5 +21,51 @@ router.post("/refresh", refreshToken as any);
 router.post("/send-verification-email", authMiddleware, sendEmailVerification);
 router.post("/verify-email", verifyEmail)
 router.get("/check", verifyUser as any);
+
+router.post("/forgot-password", async (req: Request, res: Response) => {
+    const username = req.body.username;
+    const user = await UserModel.findOne({username});
+    if(!user) {
+        res.status(404).json({message: 'No user found with the email'});
+    }
+    
+    // generate reset password token
+    const expires = moment().add(10,'minutes');
+    const payload = {
+        sub: username,
+        iat: moment().unix(),
+        exp: expires.unix(),
+        type: tokenTypes.RESET_PASSWORD
+    }
+    const resetPasswordToken = jwt.sign(payload, process.env.JWT_SECRET_KEY!);
+    await Token.create({token: resetPasswordToken, user: username, type: tokenTypes.RESET_PASSWORD, blackListed: true});
+    
+    // send the token to the user via email
+    // function to sendResetPasswordEmail(username, resetPasswordToken)
+    await emailService.sendResetPasswordEmail(username, resetPasswordToken);
+    res.status(201);
+});
+
+router.post('/reset-password', async (req: Request, res: Response) => {
+    const token = req.query.token as string;
+    const newPassword = req.body.password;
+    console.log("token: ", token);
+    console.log("[PASSWORD]", newPassword);
+    const verifiedToken = jwt.verify(token, process.env.JWT_SECRET_KEY!);
+    const tokenDoc = await Token.findOne({token, user: verifiedToken.sub, type: tokenTypes?.RESET_PASSWORD});
+    if(!tokenDoc) {
+        console.log('No token found');
+        return;
+    }
+    const user = await UserModel.findOne({username: tokenDoc.user});
+    if(!user) {
+        res.status(401).json({error:'no user found'});
+    }
+    const hashedPassword = await bcryptjs.hash(newPassword, 10);
+    await UserModel.findOneAndUpdate({username: user?.username}, {password: hashedPassword}, {new: true});
+    await Token.deleteMany({user: user?.username, token, type: tokenTypes.RESET_PASSWORD});
+    res.status(201).json({"message": 'Sucess'});
+
+})
 
 export const userRouter = router;
--- server/services/email.service.ts
+++ server/services/email.service.ts
@@ -14,6 +14,16 @@ const sendEmail = async (to: string, subject: string, text: string) => {
     await transport.sendMail(message);
 }
 
+const sendResetPasswordEmail = async (to: string, token: string) => {
+  const subject = 'Reset password';
+  // replace this url with the link to the reset password page of your front-end app
+  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
+  const text = `Dear user,
+To reset your password, click on this link: ${resetPasswordUrl}
+If you did not request any password resets, then ignore this email.`;
+  await sendEmail(to, subject, text);
+};
+
 const sendEmailVerification = async  (to: string, token: string) => {
     const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
@@ -24,4 +34,4 @@ If you did not create an account, then ignore this email.`;
   await sendEmail(to, subject, text);
 }
 
-export { sendEmail, sendEmailVerification }
+export { sendEmail, sendEmailVerification, sendResetPasswordEmail }
