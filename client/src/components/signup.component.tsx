6c3605603b838d14830e2faf6ec590c5df1bf165
Senthil Athiban <senthilathiban0@gmail.com>
2025-01-03 22:33:01 +0530

    (feat) slack auth

--- client/src/components/signup.component.tsx
+++ client/src/components/signup.component.tsx
@@ -19,6 +19,10 @@ const Signup = () => {
     console.log("res : ", res);
   }
 
+  const handleSlackLogin = () => {
+    const res = window.open("http://localhost:8080/auth/slack", "_self");
+  }
+
   const handleSubmit = async () => {
     const res = await axios.post("/api/v1/user/signup", {
       username: name,
@@ -66,7 +70,13 @@ const Signup = () => {
       {/* <button onClick={handleClick}>Verify </button> */}
       <button onClick={handleGoogleLogin}>Login in using google</button>
       <button onClick={handleGithubLogin}>Login in using github</button>
+      <button onClick={handleSlackLogin}>Login in using Slack</button>
       <button onClick={handleLogout}>Logout</button>
+      <a href="https://slack.com/oauth/v2/authorize?client_id=8209065264739.8221208121687&scope=channels:join,channels:read,chat:write,groups:read,incoming-webhook,team:read,users:read,im:read,mpim:read&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+      {/* <a href="https://slack.com/oauth/v2/authorize?client_id=8209065264739.8221208121687&scope=channels:read,chat:write,team:read,users:read,groups:read,incoming-webhook&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a> */}
+      {/* <a href="https://slack.com/oauth/v2/authorize?client_id=8209065264739.8221208121687&scope=channels:read,chat:write,team:read,users:read,groups:read&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a> */}
+      {/* <a href="https://slack.com/oauth/v2/authorize?client_id=8209065264739.8221208121687&scope=channels:read&user_scope=admin"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a> */}
+      {/* <a href="https://slack.com/oauth/v2/authorize?client_id=8209065264739.8221851186833&scope=chat:write,chat:write.public,incoming-webhook,channels:read,groups:read,mpim:read,im:read&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a> */}
     </div>
   );
 };
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import connectDb from "./db/db";
-import {userRouter} from "./routers/auth.route";
+import { userRouter } from "./routers/auth.route";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import cookieSession from "cookie-session";
@@ -9,25 +9,71 @@ import { googleRouter } from "./routers/google.route";
 import passport from "passport";
 import session from "express-session";
 import { githubRouter } from "./routers/github.route";
+import { WebClient } from "@slack/web-api";
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieSession({
+app.use(
+  cookieSession({
     name: "session",
     keys: ["auth"],
-    maxAge: 24 * 60 * 60 * 1000
-}));
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
 app.use(passport.session());
 app.use(passport.initialize());
-app.use(cors({origin: [process.env.CLIENT_URL!, "*"], credentials: true}));
-app.use(session({ secret: process.env.SESSION_SECRET!, resave: true, saveUninitialized: true }));
+app.use(cors({ origin: [process.env.CLIENT_URL!, "*"], credentials: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(cookieParser());
 app.use("/api/v1/user", userRouter);
 app.use("/", googleRouter);
 app.use("/", githubRouter);
+
+app.get("/api/v1/slack/callback", async (req, res) => {
+  try {
+    const { code } = req.query;
+    const web = new WebClient();
+    const c = code as string;
+    console.log("code: ", code);
+    // Exchange code for token
+    const response = await web.oauth.v2.access({
+      client_id: process.env.SLACK_CLIENT_ID!,
+      client_secret: process.env.SLACK_CLIENT_SECRET!,
+      code: c,
+      redirect_uri: "https://redirectmeto.com/http://localhost:8080/api/v1/slack/callback",
+    });
+
+    // Store these securely in your database
+    const workspaceToken = response.access_token;
+    console.log("workspace token:", workspaceToken);
+    const workspaceId = response?.team?.id;
+    console.log("workspace id :", workspaceId);
+
+    // Get channels list
+    const slackClient = new WebClient(workspaceToken);
+    const channels = await slackClient.conversations.list();
+    console.log("channels: ", channels);
+    res.json({
+      success: true,
+      channels: channels?.channels?.map((c: any) => ({
+        id: c.id,
+        name: c.name,
+      })),
+    });
+  } catch (error) {
+    res.json({ success: false, error: error });
+  }
+});
+
 app.listen(8080, () => {
-    console.log('server started')
-    connectDb();
+  console.log("server started");
+  connectDb();
 });
